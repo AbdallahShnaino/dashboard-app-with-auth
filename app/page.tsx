a4ac9d9d7111ce2a95bbd53b053cc256a5553a92
AbdallahShnaino <abdallah.shnaino@gmail.com>
2025-03-12 11:06:48 +0200

    go

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
-import Image from "next/image";
 import { redirect } from "next/navigation";
-import Link from "next/link";
-import styles from "./page.module.css";
+
 export default function Home() {
   redirect("/sign-up");
-  return <h1>hello</h1>;
+  return <h1>Hello I'am Farah</h1>;
 }
